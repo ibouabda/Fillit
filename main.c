48bbbbaee4279e7daabae697d880758c4e403af3
Reda TOUNSI <retounsi@e1r13p1.42.fr>
2019-02-21 17:59:38 +0100

    cas sans \n regle reste a corriger cas de fichier vide

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: retounsi <retounsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 13:26:16 by ibouabda          #+#    #+#             */
-/*   Updated: 2019/02/21 16:57:18 by retounsi         ###   ########.fr       */
+/*   Updated: 2019/02/21 17:58:16 by retounsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main(int argc, char **argv)
 	int fd_check;
 	char ***tetrim;
 	char ***tetrim_check;
+	char *test;
 	size_t i;
 	size_t k;
 
@@ -29,21 +30,28 @@ int main(int argc, char **argv)
 	fd_check = open("library.fillit", O_RDONLY);
 	fd = open(argv[1], O_RDONLY);
 	i = 0;
+	while (i < 5)
+	{
+		get_next_line(fd, &test);
+		i++;
+	}
+	i = 0;
 	k = 0;
 	tetrim = ft_3Dstrnew(26);
 	tetrim_check = ft_3Dstrnew(19);
 	ft_readtetris_check(fd_check, tetrim_check);
-	if (!(ft_readtetris_check(fd, tetrim)))
+	if (test[0] == '\0')
 	{
-		ft_putendl("Error number of tetriminos");
-		return(0);
-	}
-	if (tetrim[0][0])
-	{
-		ft_putendl("test");
-		ft_3Dputstr(tetrim);
-		ft_erase_column(tetrim);
-		ft_erase_column(tetrim_check);
+		if (!(ft_readtetris_check(fd, tetrim)))
+		{
+			ft_putendl("Error number of tetriminos");
+			return(0);
+		}
+		if (tetrim[0][0])
+		{
+			ft_erase_column(tetrim);
+			ft_erase_column(tetrim_check);
+		}
 	}
 	else
 	{
