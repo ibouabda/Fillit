a56668da89944d94e6f818fd83779efae86faa44
Reda TOUNSI <retounsi@e1r8p11.42.fr>
2019-09-06 10:15:56 +0200

    tout bon avec petites erreurs de norme

--- ft_readtetris_check.c
+++ ft_readtetris_check.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_readtetris_check.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: idris <idris@student.42.fr>                +#+  +:+       +#+        */
+/*   By: retounsi <retounsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 13:29:07 by ibouabda          #+#    #+#             */
-/*   Updated: 2019/09/06 09:59:45 by idris            ###   ########.fr       */
+/*   Updated: 2019/09/06 10:14:26 by retounsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		ft_verif_line_check(char *line)
+int		ft_verif(char *line)
 {
 	int i;
 
@@ -26,10 +26,10 @@ int		ft_verif_line_check(char *line)
 	return (0);
 }
 
-void	ft_initialize(size_t *nb_line, size_t *y, size_t *ntetrim, size_t *l)
+void	ft_initialize(size_t *nl, size_t *y, size_t *ntetrim, size_t *l)
 {
 	*l = 0;
-	*nb_line = 0;
+	*nl = 0;
 	*y = 0;
 	*ntetrim = 0;
 }
@@ -51,37 +51,37 @@ int		ft_free_return(char *line)
 	return (0);
 }
 
-void ft_initialize_2(char ***tetrim, char ***tetrim_c)
+void	ft_initialize_2(char ***tetrim, char ***tetrim_c)
 {
 	tetrim[0] = ft_2dstrnew(4);
 	tetrim_c[0] = ft_2dstrnew(4);
 }
+
 int		ft_readtetris_check(int fd, char ***tetrim, char ***tetrim_c)
 {
 	char	*line;
 	size_t	y;
 	size_t	l;
-	size_t	nb_line;
+	size_t	nl;
 	size_t	ntetrim;
 
-	ft_initialize(&nb_line, &y, &ntetrim, &l);
+	ft_initialize(&nl, &y, &ntetrim, &l);
 	ft_initialize_2(tetrim, tetrim_c);
-	while (++nb_line && get_next_line(fd, &line))
+	while (++nl && get_next_line(fd, &line))
 	{
-		if (!line[0] && nb_line % 5 == 0)
+		if (!line[0] && nl % 5 == 0)
 		{
 			free(line);
 			if (!y || !(ft_new_tetrim_check(tetrim, tetrim_c, &ntetrim, &y)))
 				return (0);
 			if ((l = 0) == 0 && get_next_line(fd, &line))
-				nb_line++;
+				nl++;
 		}
-		if ((!line[0] && nb_line % 5 != 0) || ft_strlen(line) != 4 || y > 4) // why 4 et l > 4 necessaire ?
+		if ((!line[0] && nl % 5 != 0) || ft_strlen(line) != 4 || y > 4 || l > 4)
 			return (ft_free_return(line));
-		tetrim_c[ntetrim][l++] = ft_strdup(line);
-		if (ft_verif_line_check(line))
+		if ((tetrim_c[ntetrim][l++] = ft_strdup(line)) && ft_verif(line))
 			tetrim[ntetrim][y++] = ft_strdup(line);
 		free(line);
 	}
-	return (tetrim[0][0] != NULL && nb_line % 5 == 0 && y > 0);
+	return (tetrim[0][0] != NULL && nl % 5 == 0 && y > 0);
 }
