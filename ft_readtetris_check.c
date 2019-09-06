ca4ab4359e1c2b6368029bb448572629fcaa7ac4
Reda TOUNSI <retounsi@e1r8p11.42.fr>
2019-09-06 12:14:07 +0200

    ok

--- ft_readtetris_check.c
+++ ft_readtetris_check.c
@@ -6,7 +6,7 @@
 /*   By: retounsi <retounsi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 13:29:07 by ibouabda          #+#    #+#             */
-/*   Updated: 2019/09/06 12:08:01 by retounsi         ###   ########.fr       */
+/*   Updated: 2019/09/06 12:12:51 by retounsi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ int		ft_new_tetrim_check(char ***tet, char ***tet_c, size_t *ntet, size_t *y)
 
 int		ft_free_return(char **line, size_t nl)
 {
-	if (*line && nl % 5 == 0)
+	if (nl % 5 != 0 && *line)
 	{
 		if (*line[0])
 			free(*line);
@@ -67,7 +67,7 @@ int		ft_readtetris_check(int fd, char ***tetrim, char ***tetrim_c)
 	ft_initialize_2(tetrim, tetrim_c);
 	while (++nl && get_next_line(fd, &line))
 	{
-		if (!line[0] && nl % 5 == 0)
+		if ( nl % 5 == 0 && !line[0])
 		{
 			free(line);
 			if (!y || !(ft_new_tetrim_check(tetrim, tetrim_c, &ntetrim, &y)))
@@ -75,7 +75,7 @@ int		ft_readtetris_check(int fd, char ***tetrim, char ***tetrim_c)
 			if ((l = 0) == 0 && get_next_line(fd, &line))
 				nl++;
 		}
-		if ((nl % 5 != 0 && !line[0]) || ft_strlen(line) != 4 || y > 4 || l > 4)
+		if ((nl % 5 != 0 && !line[0]) || ft_strlen(line) != 4 || y > 3 || l > 3)
 			return (ft_free_return(&line, nl));
 		if ((tetrim_c[ntetrim][l++] = ft_strdup(line)) && ft_verif(line))
 			tetrim[ntetrim][y++] = ft_strdup(line);
