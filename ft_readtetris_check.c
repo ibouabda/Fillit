be2a96809a1b307f136fd5a332494fb857dc2173
Idris BOUABDALLAH <idro30@hotmail.fr>
2019-09-06 16:29:12 +0200

    ok

--- ft_readtetris_check.c
+++ ft_readtetris_check.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_readtetris_check.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: retounsi <retounsi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ibouabda <ibouabda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/06 13:29:07 by ibouabda          #+#    #+#             */
-/*   Updated: 2019/09/06 15:54:07 by retounsi         ###   ########.fr       */
+/*   Updated: 2019/09/06 16:23:25 by ibouabda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int		ft_free_return(char **line)
 
 int		ft_readtetris_check(int fd, char ***tetrim, char ***tetrim_c)
 {
-	char	*line;
+	char	*li;
 	size_t	y;
 	size_t	l;
 	size_t	nl;
@@ -61,21 +61,21 @@ int		ft_readtetris_check(int fd, char ***tetrim, char ***tetrim_c)
 
 	ft_initialize(&nl, &y, &ntetrim, &l);
 	ft_initialize_2(tetrim, tetrim_c);
-	while (++nl && get_next_line(fd, &line))
+	while (++nl && get_next_line(fd, &li))
 	{
-		if (nl % 5 == 0 && !line[0])
+		if (nl % 5 == 0 && !li[0])
 		{
-			ft_strdel(&line);
+			ft_strdel(&li);
 			if (!y || !(ft_new_tetrim_check(tetrim, tetrim_c, &ntetrim, &y)))
 				return (0);
-			if ((l = 0) == 0 && get_next_line(fd, &line))
+			if ((l = 0) == 0 && get_next_line(fd, &li))
 				nl++;
 		}
-		if (line && ((nl % 5 != 0 && !line[0]) || ft_strlen(line) != 4 || y > 3 || l > 3))
-			return (ft_free_return(&line));
-		if (line && ((tetrim_c[ntetrim][l++] = ft_strdup(line)) && ft_verif(line)))
-			tetrim[ntetrim][y++] = ft_strdup(line);
-		ft_strdel(&line);
+		if (li && ((nl % 5 != 0 && !li[0]) || ft_strlen(li) != 4 || l > 3))
+			return (ft_free_return(&li));
+		if (li && ((tetrim_c[ntetrim][l++] = ft_strdup(li)) && ft_verif(li)))
+			tetrim[ntetrim][y++] = ft_strdup(li);
+		ft_strdel(&li);
 	}
 	return (tetrim[0][0] != NULL && nl % 5 == 0 && y > 0);
 }
